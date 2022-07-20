d57e90e3a3012b81c8c5c6d96fcec404eae74eb2
eugeniahm <61255729+eugeniahm@users.noreply.github.com>
2022-07-20 19:53:21 -0400

    Create learningC++.cpp

---
+++ calculator/learningC++.cpp
@@ -0,0 +1,18 @@
+#include "learningHeaderFiles.h"
+#include <iostream>
+
+void SomeClass::DoNothing()
+{
+    return;
+}
+
+void SomeClass::DoSomething()
+{
+    ThisThing = 0;
+    AnotherThing = true;
+}
+
+int main()
+{
+    return 0;
+}
